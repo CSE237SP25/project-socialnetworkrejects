78c51473277309e69b7f2940315aa54be4eb9cec
Tyler Dinh <92825742+tylerdinh@users.noreply.github.com>
2025-03-19 11:13:13 -0500

    menu functionality

---
+++ src/bankapp/Main.java
@@ -0,0 +1,13 @@
+package bankapp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Menu m = new Menu();
+		m.runStartingConfiguration();
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
