b31cf6b7079f30b8d13964e69e1d918451170ff6
Tyler Dinh <92825742+tylerdinh@users.noreply.github.com>
2025-04-06 15:27:53 -0500

    Implemented base fraudulent/unusual transaction handling

--- src/bankapp/BankAccount.java
+++ src/bankapp/BankAccount.java
@@ -1,9 +1,11 @@
 package bankapp;
 public class BankAccount {
     private double balance;
+
+    private double fraudThreshold = 10000;
     
     public BankAccount() {
-        this.balance = 0;  // New account opens with $0
+        this.balance = 0;  // New account opens with re$0
         //Potential New Idea: Add account minimum like real bank accounts
     }
     /**
@@ -14,6 +16,11 @@ public class BankAccount {
         if (amount < 0) {
             throw new IllegalArgumentException("Deposit amount invalid.");
         }
+        if (isFraudulentTransaction(amount)) {
+            throw new IllegalArgumentException("Potential fraudulent transaction detected, please input less than $" 
+                                               + this.fraudThreshold + " for deposit.");
+        }
+
         this.balance += amount;
     }
     /**
@@ -24,6 +31,10 @@ public class BankAccount {
         if (amount < 0 || amount > this.balance) {
             throw new IllegalArgumentException("Withdrawal amount invalid.");
         }
+        if (isFraudulentTransaction(amount)) {
+            throw new IllegalArgumentException("Potential fraudulent transaction detected, please input less than $" 
+                                               + this.fraudThreshold + " for withdrawal.");
+        }
         this.balance -= amount;
     }
     
@@ -31,6 +42,10 @@ public class BankAccount {
     	if (amount < 0 || amount > this.balance) {
     		throw new IllegalArgumentException("Transfer amount invalid");
     	}
+        if (isFraudulentTransaction(amount)) {
+            throw new IllegalArgumentException("Potential fraudulent transaction detected, please input less than $" 
+                                               + this.fraudThreshold + " for transfer.");
+        }
     	otherAccount.deposit(amount);
     	this.withdraw(amount);
     }
@@ -40,4 +55,10 @@ public class BankAccount {
     public double getCurrentBalance() {
         return this.balance;
     }
+
+    public boolean isFraudulentTransaction(double amount) {
+        //Change the value associated with fraudThreshold as needed for fraud detection
+        return amount > this.fraudThreshold;
+    }
+
 }
